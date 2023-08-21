61e01af6966ab0ff606f295dad3ccc95fcb2ca44
ainy06 <142175236+ainy06@users.noreply.github.com>
2023-08-21 18:10:24 +0200

    learn range

---
+++ 04_list/for_range.py
@@ -0,0 +1,12 @@
+# use for loop and range
+for i in range(5):
+    print(str(i) + "= repeat variable")
+print()
+
+for i in range(5, 10):
+    print(str(i) + "= repeat variable")
+print()
+
+for i in range(0, 10, 3):
+    print(str(i) + "= repeat variable")
+print()
